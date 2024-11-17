18c605d71c3f486ba8997f7343c4f5040ad36a50
alexlanxy <33140761+alexlanxy@users.noreply.github.com>
2024-11-17 10:00:51 -0600

    remove Vmap

--- model.py
+++ model.py
@@ -1,46 +1,134 @@
-from typing import Any, Dict, List, Optional, Tuple, Union, Callable
-
+from typing import List, Optional, Tuple, Union, Callable
 import einops as E
 import torch
 from torch import nn
-from torch.nn import init
-
-import warnings
-
 from pydantic import validate_arguments
 
-size2t = Union[int, Tuple[int, int]]
+SizeType = Union[int, Tuple[int, int]]
 
-def vmap(module: Callable, x: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-    batch_size, group_size, *_ = x.shape
-    grouped_input = E.rearrange(x, "B S ... -> (B S) ...")
-    grouped_output = module(grouped_input, *args, **kwargs)
-    output = E.rearrange(
-        grouped_output, "(B S) ... -> B S ...", B=batch_size, S=group_size
-    )
-    return output
-
-
-class Vmap(nn.Module):
-    def __init__(self, module: nn.Module):
+class UniverSeg(nn.Module):
+    def __init__(
+        self,
+        encoder_blocks: List[Tuple[int, int]],
+        decoder_blocks: Optional[List[Tuple[int, int]]] = None,
+    ):
         super().__init__()
-        self.vmapped = module
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.downsample = nn.MaxPool2d(2, 2)
+        self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
 
-        return vmap(self.vmapped, x)
+        decoder_blocks = decoder_blocks or encoder_blocks[-2::-1]
+
+        # Block 1: Encoder Block 1
+        cross_features, conv_features = encoder_blocks[0]
+        self.encoder_block1 = CrossBlock((1, 2), cross_features, conv_features)
+
+        # Block 2: Encoder Block 2
+        cross_features, conv_features = encoder_blocks[1]
+        self.encoder_block2 = CrossBlock(conv_features, cross_features, conv_features)
+
+        # Block 3: Encoder Block 3
+        cross_features, conv_features = encoder_blocks[2]
+        self.encoder_block3 = CrossBlock(conv_features, cross_features, conv_features)
+
+        # Block 4: Encoder Block 4
+        cross_features, conv_features = encoder_blocks[3]
+        self.encoder_block4 = CrossBlock(conv_features, cross_features, conv_features)
+
+        # Decoder initialization
+        skip_channels = [block[1] for block in encoder_blocks[-2::-1]]
+
+        # Block 5: Decoder Block 1
+        cross_features, conv_features = decoder_blocks[0]
+        skip_connection = skip_channels[0]
+        self.decoder_block1 = CrossBlock(conv_features + skip_connection, cross_features, conv_features)
+
+        # Block 6: Decoder Block 2
+        cross_features, conv_features = decoder_blocks[1]
+        skip_connection = skip_channels[1]
+        self.decoder_block2 = CrossBlock(conv_features + skip_connection, cross_features, conv_features)
+
+        # Block 7: Decoder Block 3
+        cross_features, conv_features = decoder_blocks[2]
+        skip_connection = skip_channels[2]
+        self.decoder_block3 = CrossBlock(conv_features + skip_connection, cross_features, conv_features)
+
+        # Output Convolution
+        self.out_conv = ConvOp(conv_features, 1, kernel_size=1)
+
+    def forward(self, target_image, support_images, support_labels):
+        # Prepare inputs
+        target_tensor = E.rearrange(target_image, "B 1 H W -> B 1 1 H W")
+        support_tensor = torch.cat([support_images, support_labels], dim=2)
+
+        # Skip connections
+        skip_connections = []
+
+        # Block 1: Encoder Block 1
+        target_tensor, support_tensor = self.encoder_block1(target_tensor, support_tensor)
+        skip_connections.append((target_tensor, support_tensor))
+        target_tensor = vmap(self.downsample, target_tensor)
+        support_tensor = vmap(self.downsample, support_tensor)
+
+        # Block 2: Encoder Block 2
+        target_tensor, support_tensor = self.encoder_block2(target_tensor, support_tensor)
+        skip_connections.append((target_tensor, support_tensor))
+        target_tensor = vmap(self.downsample, target_tensor)
+        support_tensor = vmap(self.downsample, support_tensor)
+
+        # Block 3: Encoder Block 3
+        target_tensor, support_tensor = self.encoder_block3(target_tensor, support_tensor)
+        skip_connections.append((target_tensor, support_tensor))
+        target_tensor = vmap(self.downsample, target_tensor)
+        support_tensor = vmap(self.downsample, support_tensor)
+
+        # Block 4: Encoder Block 4
+        target_tensor, support_tensor = self.encoder_block4(target_tensor, support_tensor)
+
+        # Block 5: Decoder Block 1
+        target_skip, support_skip = skip_connections.pop()
+        target_tensor = torch.cat([vmap(self.upsample, target_tensor), target_skip], dim=2)
+        support_tensor = torch.cat([vmap(self.upsample, support_tensor), support_skip], dim=2)
+        target_tensor, support_tensor = self.decoder_block1(target_tensor, support_tensor)
+
+        # Block 6: Decoder Block 2
+        target_skip, support_skip = skip_connections.pop()
+        target_tensor = torch.cat([vmap(self.upsample, target_tensor), target_skip], dim=2)
+        support_tensor = torch.cat([vmap(self.upsample, support_tensor), support_skip], dim=2)
+        target_tensor, support_tensor = self.decoder_block2(target_tensor, support_tensor)
+
+        # Block 7: Decoder Block 3
+        target_skip, support_skip = skip_connections.pop()
+        target_tensor = torch.cat([vmap(self.upsample, target_tensor), target_skip], dim=2)
+        support_tensor = torch.cat([vmap(self.upsample, support_tensor), support_skip], dim=2)
+        target_tensor, support_tensor = self.decoder_block3(target_tensor, support_tensor)
+
+        # Output Convolution
+        target_tensor = E.rearrange(target_tensor, "B 1 C H W -> B C H W")
+        target_tensor = self.out_conv(target_tensor)
+
+        return target_tensor
+
+def vmap(module: Callable, input_tensor: torch.Tensor, *args, **kwargs) -> torch.Tensor:
+    batch_size, group_size, *_ = input_tensor.shape
+    flat_input = E.rearrange(input_tensor, "B G ... -> (B G) ...")
+    flat_output = module(flat_input, *args, **kwargs)
+    grouped_output = E.rearrange(
+        flat_output, "(B G) ... -> B G ...", B=batch_size, G=group_size
+    )
+    return grouped_output
 
 
 class CrossConv2d(nn.Conv2d):
     @validate_arguments
     def __init__(
         self,
-        in_channels: size2t,
-        out_channels: int,
-        kernel_size: size2t,
-        stride: size2t = 1,
-        padding: size2t = 0,
-        dilation: size2t = 1,
+        input_channels: SizeType,
+        output_channels: int,
+        kernel_size: SizeType,
+        stride: SizeType = 1,
+        padding: SizeType = 0,
+        dilation: SizeType = 1,
         groups: int = 1,
         bias: bool = True,
         padding_mode: str = "zeros",
@@ -48,14 +136,14 @@ class CrossConv2d(nn.Conv2d):
         dtype=None,
     ) -> None:
 
-        if isinstance(in_channels, (list, tuple)):
-            concat_channels = sum(in_channels)
+        if isinstance(input_channels, (list, tuple)):
+            combined_channels = sum(input_channels)
         else:
-            concat_channels = 2 * in_channels
+            combined_channels = 2 * input_channels
 
         super().__init__(
-            in_channels=concat_channels,
-            out_channels=out_channels,
+            in_channels=combined_channels,
+            out_channels=output_channels,
             kernel_size=kernel_size,
             stride=stride,
             padding=padding,
@@ -67,145 +155,78 @@ class CrossConv2d(nn.Conv2d):
             dtype=dtype,
         )
 
-    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-        B, Sx, *_ = x.shape
-        _, Sy, *_ = y.shape
+    def forward(self, target_tensor: torch.Tensor, support_tensor: torch.Tensor) -> torch.Tensor:
+        batch_size, target_group, *_ = target_tensor.shape
+        _, support_group, *_ = support_tensor.shape
 
-        xs = E.repeat(x, "B Sx Cx H W -> B Sx Sy Cx H W", Sy=Sy)
-        ys = E.repeat(y, "B Sy Cy H W -> B Sx Sy Cy H W", Sx=Sx)
+        expanded_target = E.repeat(target_tensor, "B T C H W -> B T S C H W", S=support_group)
+        expanded_support = E.repeat(support_tensor, "B S C H W -> B T S C H W", T=target_group)
 
-        xy = torch.cat([xs, ys], dim=3,)
+        concatenated_inputs = torch.cat([expanded_target, expanded_support], dim=3)
 
-        batched_xy = E.rearrange(xy, "B Sx Sy C2 H W -> (B Sx Sy) C2 H W")
-        batched_output = super().forward(batched_xy)
+        flat_inputs = E.rearrange(concatenated_inputs, "B T S C H W -> (B T S) C H W")
+        flat_output = super().forward(flat_inputs)
 
-        output = E.rearrange(
-            batched_output, "(B Sx Sy) Co H W -> B Sx Sy Co H W", B=B, Sx=Sx, Sy=Sy
+        grouped_output = E.rearrange(
+            flat_output, "(B T S) C H W -> B T S C H W", B=batch_size, T=target_group, S=support_group
         )
-        return output
+        return grouped_output
 
 
 class ConvOp(nn.Sequential):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: size2t = 3):
+    def __init__(self, input_channels: int, output_channels: int, kernel_size: SizeType = 3):
         super().__init__()
         self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
+            input_channels,
+            output_channels,
             kernel_size=kernel_size,
             padding=kernel_size // 2,
             padding_mode="zeros",
             bias=True,
         )
 
+
 class CrossOp(nn.Module):
     @validate_arguments
-    def __init__(self, in_channels: size2t, out_channels: int, kernel_size: size2t = 3):
+    def __init__(self, input_channels: SizeType, output_channels: int, kernel_size: SizeType = 3):
         super().__init__()
         self.cross_conv = CrossConv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
+            input_channels=input_channels,
+            output_channels=output_channels,
             kernel_size=kernel_size,
             padding=kernel_size // 2,
         )
 
-    def forward(self, target, support):
-        interaction = self.cross_conv(target, support).squeeze(dim=1)
-        new_target = interaction.mean(dim=1, keepdims=True)
-        return new_target, interaction
+    def forward(self, target_tensor, support_tensor):
+        interactions = self.cross_conv(target_tensor, support_tensor).squeeze(dim=1)
+        aggregated_target = interactions.mean(dim=1, keepdims=True)
+        return aggregated_target, interactions
+
 
 class CrossBlock(nn.Module):
     @validate_arguments
     def __init__(
         self,
-        in_channels: size2t,
+        input_channels: SizeType,
         cross_features: int,
         conv_features: Optional[int] = None,
-        cross_kws: Optional[Dict[str, Any]] = None,
-        conv_kws: Optional[Dict[str, Any]] = None,
     ):
         super().__init__()
 
         conv_features = conv_features or cross_features
-        cross_kws = cross_kws or {}
-        conv_kws = conv_kws or {}
 
-        self.cross = CrossOp(in_channels, cross_features, **cross_kws)
-        self.target = Vmap(ConvOp(cross_features, conv_features, **conv_kws))
-        self.support = Vmap(ConvOp(cross_features, conv_features, **conv_kws))
+        self.cross = CrossOp(input_channels, cross_features)
+        self.target = ConvOp(cross_features, conv_features)
+        self.support = ConvOp(cross_features, conv_features)
 
-    def forward(self, target, support):
-        target, support = self.cross(target, support)
-        target = self.target(target)
-        support = self.support(support)
-        return target, support
+    def forward(self, target_tensor, support_tensor):
+        target_tensor, support_tensor = self.cross(target_tensor, support_tensor)
+        target_tensor = vmap(self.target.forward, target_tensor)
+        support_tensor = vmap(self.support.forward, support_tensor)
+        return target_tensor, support_tensor
 
 
-class UniverSeg(nn.Module):
-    def __init__(
-        self,
-        encoder_blocks: List[Tuple[int, int]],  # Expecting a list of 2-tuples
-        decoder_blocks: Optional[List[Tuple[int, int]]] = None,
-    ):
-        super().__init__()
 
-        self.downsample = nn.MaxPool2d(2, 2)
-        self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
-
-        self.enc_blocks = nn.ModuleList()
-        self.dec_blocks = nn.ModuleList()
-
-        decoder_blocks = decoder_blocks or encoder_blocks[-2::-1]
-
-        # Remove nonlinearity from block_kws
-        block_kws = dict(cross_kws={})
-
-        in_ch = (1, 2)
-        out_channels = 1
-        out_activation = None
-
-        # Encoder
-        skip_outputs = []
-        for (cross_ch, conv_ch) in encoder_blocks:
-            block = CrossBlock(in_ch, cross_ch, conv_ch, **block_kws)
-            in_ch = conv_ch
-            self.enc_blocks.append(block)
-            skip_outputs.append(in_ch)
-
-        # Decoder
-        skip_chs = skip_outputs[-2::-1]
-        for (cross_ch, conv_ch), skip_ch in zip(decoder_blocks, skip_chs):
-            block = CrossBlock(in_ch + skip_ch, cross_ch, conv_ch, **block_kws)
-            in_ch = conv_ch
-            self.dec_blocks.append(block)
-
-        self.out_conv = ConvOp(
-            in_ch, out_channels, kernel_size=1
-        )
-
-    def forward(self, target_image, support_images, support_labels):
-        target = E.rearrange(target_image, "B 1 H W -> B 1 1 H W")
-        support = torch.cat([support_images, support_labels], dim=2)
-
-        pass_through = []
-
-        for i, encoder_block in enumerate(self.enc_blocks):
-            target, support = encoder_block(target, support)
-            if i == len(self.enc_blocks) - 1:
-                break
-            pass_through.append((target, support))
-            target = vmap(self.downsample, target)
-            support = vmap(self.downsample, support)
-
-        for decoder_block in self.dec_blocks:
-            target_skip, support_skip = pass_through.pop()
-            target = torch.cat([vmap(self.upsample, target), target_skip], dim=2)
-            support = torch.cat([vmap(self.upsample, support), support_skip], dim=2)
-            target, support = decoder_block(target, support)
-
-        target = E.rearrange(target, "B 1 C H W -> B C H W")
-        target = self.out_conv(target)
-
-        return target
 
 
 def universeg() -> nn.Module:
